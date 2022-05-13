a69c41acd5388cec8652737a9121678ae579b662
Eraze86 <therese.rosvall@hotmail.se>
2022-05-13 14:39:12 +0200

    small fix

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 
+
 //visa logga in
 router.get('/', function(req, res, next) {
   let loggin = `<form action="/" method="post">användarnamn: <input type="text" name="userName"/> Lösenord: <input type="text" name="passWord"/><button type="submit">Logga in</button></form><br/><br/>`
@@ -11,7 +12,7 @@ res.send(loggin + addUsers)
 });
 //visa lägg till ny användare
 router.get('/addUser', function(req, res, next) {
-  let addUser = `<form action="addUser" method="post">Användarnamn: <input name="userName"><br/> Lösenord: <input name="passWord"> <br/> <button type="submit">Lägg till</button></form>`
+  let addUser = `<form action="/addUser" method="post">Användarnamn: <input type="text" name="userName"><br/> Lösenord: <input  type="text" name="passWord"> <br/> <button type="submit">Lägg till</button></form>`
     res.send(addUser)
 
 });
@@ -26,7 +27,8 @@ if(err){
 let user = JSON.parse(data);
 
 if(user.userName == req.body.userName && user.passWord == req.body.passWord){
-console.log("du är inloggad")
+  let loggedIn = `du är inloggad`
+  res.send(loggedIn)
 }else{
   console.log("där blev de fel")
 }
@@ -51,7 +53,7 @@ user.push(newUser)
 
 //spara
 
-fs.writeFile("users.json", JSON.stringify(newUser, null, 2), (err) => {
+fs.writeFile("users.json", JSON.stringify(user, null, 2), (err) => {
   if(err){
     console.log("hell no", err)
   }
